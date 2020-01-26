c9af18a0fc0bb846d516939f5ad6aae8a6f14ff7
kproshakov <kproshakov@gmail.com>
2020-01-26 13:34:14 -0500

    deleted

--- parking-demo/path.py
+++
@@ -1,8 +0,0 @@
-from location import *
-
-class Path:
-    def __init__(self, origin= Location(), destination=Location(0, 0)):
-        self.origin=origin
-        self.destination=destination
-    
-
